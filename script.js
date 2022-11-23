59a7af36350cbaa2eddb62a1573146fd0893db2d
AmirBaa <amirbalic1408@hotmail.com>
2022-11-23 22:32:59 +0100

    final version

--- script.js
+++ script.js
@@ -3,12 +3,25 @@ const quoteText = document.getElementById('quote')
 const authorText = document.getElementById('author')
 const twitterBtn = document.getElementById('twitter')
 const newQuoteBtn = document.getElementById('new-quote')
+const loader = document.getElementById('loader')
 
 let apiQuotes = []      //making a global variable so we can use it across the script
 
+//SHow loading
+function loading() {
+    loader.hidden = false
+    quoteContainer.hidden = true
+}
+
+//Hide loading
+function complete() {
+    quoteContainer.hidden = false
+    loader.hidden = true
+}
 //SHow new quote
 
 function newQuote() {
+    loading()
 
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)]  //logic to pick a random Quote from apiquotes array
     //CHeck if Author field is blank and replace with unknown
@@ -23,12 +36,16 @@ function newQuote() {
     } else {
         quoteText.classList.remove('long-quote')
     }
+
+    //Set quote, hide loader
     quoteText.textContent = quote.text
+    complete()
 }
 
 // Get quotes from API
 
 async function getQuotes() {
+    loading()
     const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
     try {
         const response = await fetch(apiUrl);
@@ -50,5 +67,7 @@ newQuoteBtn.addEventListener('click', newQuote)
 twitterBtn.addEventListener('click', tweetQuote)
 
 // //On Load
+
 getQuotes();
 //newQuote()
+
