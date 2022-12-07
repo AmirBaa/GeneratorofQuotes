3938b083307482f668b8d20741f78b1dbe13a082
AmirBaa <amirbalic1408@hotmail.com>
2022-12-07 20:46:18 +0100

    renaming

---
+++ cypress/e2e/quoteGenerator.cy.js
@@ -0,0 +1,31 @@
+/// <reference types="Cypress" />
+
+describe('Quote Generator', () => {
+    it('calls API', () => {
+        cy.request('https://jacintodesign.github.io/quotes-api/data/quotes.json')
+            .its('status')
+            .should('eq', 200)
+        cy.request('https://jacintodesign.github.io/quotes-api/data/quotes.json')
+            .its('body')
+            .should('have.length', '8261')
+        // cy.request ('https://jacintodesign.github.io/quotes-api/data/quotes.json')
+        // .its('body')
+        // .each(value => {
+        //     expect(value).to.have.all.keys('text', 'author', 'tag')  To check if the body has those 3 values - it takes to long time
+    })
+})
+
+it('generates quotes', () => {
+    cy.visit('http://127.0.0.1:5501/index.html')
+    cy.get('#loader').should('be.visible')
+    cy.get('#new-quote').click()
+    cy.get('.quote-text').should('be.visible')
+    cy.get('#loader').should('not.be.visible')
+    cy.get('#new-quote').click()
+    cy.get('#loader').should('not.be.visible')
+    cy.get('.quote-text').should('be.visible')
+    cy.get('#new-quote').click()
+    cy.get('.quote-text').should('be.visible')
+    cy.get('#loader').should('not.be.visible')
+    cy.get('#twitter').trigger('mouseover')
+})
